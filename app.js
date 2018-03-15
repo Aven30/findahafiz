7e09ac588414882b87ebde320f93de0b56075f06
ahmad <aven30@gmail.com>
2018-03-15 15:04:12 -0700

    better setup

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const path = require("path");
 
 // Set up the express app
 const app = express();
+const paths = {
+    index: path.resolve(__dirname, 'src/client','index.html'),
+    dist: path.resolve(__dirname, 'dist'),
+}
 
 // Log requests to the console.
 app.use(logger('dev'));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+// app.get('*', (req, res) => res.status(200).send({
+//   message: 'Welcome to the beginning of nothingness.',
+// }));
+
+app.use(express.static(paths.dist))
+
+app.get('*', (req, res) => res.sendFile(paths.index))
+app.get('bundle.js', (req, res) => res.sendFile(paths.dist + '/dist/bundle.js'))
 
 module.exports = app;
---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+render((
+    <div>Hello Everyone!</div>
+), document.getElementById('root'));
+
+console.log('hi');
