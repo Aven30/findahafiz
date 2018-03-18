9c9ce720d7695c808783015792858998a735b55e
ahmad <aven30@gmail.com>
2018-03-17 23:10:58 -0700

    partial components

---
+++ src/client/components/partials/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignUp = () => (
+    <div>
+        <a href="/Login">Login</a> if you already have an account
+        {/*<Button color="primary" size="lg" block>SIGN UP WITH FACEBOOK</Button>*/}
+        {/*<Button color="primary" size="lg" block>SIGN UP WITH GMAIL</Button>*/}
+        {/*<Button color="primary" size="lg" block>SIGN UP WITH EMAIL</Button>*/}
+    </div>
+)
+
+export default SignUp
