a15545590fc5e95a5d1e5e79bc020615bfc7eb89
ahmad <aven30@gmail.com>
2018-04-06 06:32:24 -0700

    validation complete

--- src/client/components/partials/SignUp/SignUpForm.jsx
+++ src/client/components/partials/SignUp/SignUpForm.jsx
@@ -1,79 +1,105 @@
 import React from 'react'
-import {Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
+import {Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap'
 import {validateEmail, validateRequired} from '../../../utilities/validations'
 
 class SignUpForm extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            inputErrors: {
-                emailError: false,
-                firstNameError: false,
-                lastNameError: false,
-            }
-        };
+        this.state = {form: {}, errors: {}}
 
+        this.setFirstName = this.setFirstName.bind(this)
+        this.setLastName = this.setLastName.bind(this)
+        this.setEmail = this.setEmail.bind(this)
+        this.setPassword = this.setPassword.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
-        this.checkEmailValidation = this.checkEmailValidation.bind(this)
-        this.clearErrors = this.clearErrors.bind(this)
+        this.validateFields = this.validateFields.bind(this)
+    }
+
+    setEmail(e) {
+        this.state.form.email = e.target.value;
+    }
+
+    setPassword(e) {
+        this.state.form.password = e.target.value;
+    }
+
+    setFirstName(e) {
+        this.state.form.firstName = e.target.value;
+    }
+
+    setLastName(e) {
+        this.state.form.lastName = e.target.value;
+    }
+
+    checkEmail(e) {
+        this.state.errors.email = !validateEmail(this.state.form.email)
+        this.forceUpdate()
     }
 
     handleSubmit(e) {
-        console.log(this.props);
+
+        if (this.validateFields()) {
+            console.log('NOT GONNA SUBMIT!')
+        } else {
+            console.log('LET US SUBMIT!')
+        }
     }
 
-    checkEmailValidation(e) {
-        console.log(!validateEmail(e.target.value));
+    validateFields() {
+
+        let firstNameErr = validateRequired(this.state.form.firstName)
+        let lastNameErr = validateRequired(this.state.form.lastName)
+        let emailErr = !validateEmail(this.state.form.email)
+        let passwordErr = validateRequired(this.state.form.password)
+
         this.setState({
-            inputErrors: {
-                emailError: !validateEmail(e.target.value)
+            errors: {
+                email: emailErr,
+                firstName: firstNameErr,
+                lastName: lastNameErr,
+                password: passwordErr
             }
+
         })
-    }
-
-    checkFirstNameRequired() {
-
-    }
-
-    checkLastNameRequired() {
-
-    }
-
-    clearErrors() {
-        this.setState({
-            inputErrors: {
-                emailError: false,
-            }
-        })
+        console.log(this.state)
+        return firstNameErr || lastNameErr || emailErr || passwordErr;
     }
 
     render() {
-
-        let emailError = this.state.inputErrors.emailError;
-        let firstNameError = this.state.inputErrors.firstNameError;
-        let lastNameError = this.state.inputErrors.lastNameError;
 
         return <Form>
             <FormGroup>
                 <Label for="signup-email" className="mb-0">Email</Label>
                 <Input type="email" name="email" id="signup-email"
-                       className={emailError ? 'is-invalid' : ''}
-                       onBlur={this.checkEmailValidation} onChange={this.clearErrors}/>
-                <span id="emailErrMsg" class="invalid-feedback">Invalid Email</span>
+                       onBlur={(e) => this.checkEmail(e)}
+                       className={this.state.errors.email ? 'is-invalid' : ''}
+                       onChange={this.setEmail}/>
+                <span class="invalid-feedback">Invalid Email</span>
             </FormGroup>
             <FormGroup>
                 <Label for="signup-password" className="mb-0">Password</Label>
-                <Input type="password" name="password" id="signup-password"/>
+                <Input type="password" name="password" className={this.state.errors.password ? 'is-invalid' : ''}
+                       onChange={this.setPassword}
+                       id="signup-password"/>
+                <span class="invalid-feedback">Required</span>
             </FormGroup>
             <FormGroup className="row">
                 <div className="col-md-6">
-                    <Label for="signup-email" className="mb-0">First name</Label>
-                    <Input type="text" className={emailError ? 'is-invalid' : ''} name="text" id="signup-email"/>
+                    <Label for="signup-firstname" className="mb-0">First name</Label>
+                    <Input type="text"
+                           onChange={this.setFirstName}
+                           className={this.state.errors.firstName ? 'is-invalid' : ''}
+                           name="firstName" id="signup-email"/>
+                    <span class="invalid-feedback">Required</span>
                 </div>
                 <div className="col-md-6">
-                    <Label for="signup-email" className="mb-0">Last name</Label>
-                    <Input type="text" className={emailError ? 'is-invalid' : ''} name="text" id="signup-email"/>
+                    <Label for="signup-lastname" className="mb-0">Last name</Label>
+                    <Input type="text"
+                           onChange={this.setLastName}
+                           className={this.state.errors.lastName ? 'is-invalid' : ''}
+                           name="lastName" id="signup-lastname"/>
+                    <span class="invalid-feedback">Required</span>
                 </div>
             </FormGroup>
             <Button block onClick={this.handleSubmit}>Submit</Button>
